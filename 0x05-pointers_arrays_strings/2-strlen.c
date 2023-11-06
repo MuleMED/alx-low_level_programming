fa62121c50c4c91aedc560caeb96bc036c48c7d3
Mule Samuel <117080154+MuleMED@users.noreply.github.com>
2023-11-07 02:06:14 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * _strlen - return the length of the string
- *
- * @s: is out test value to pass as an array
- * Return: returns the length
- */
 int _strlen(char *s) {
   int i;
 
