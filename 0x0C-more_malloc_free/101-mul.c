d9760e4884d9eda2cf10ebb7e42ebc3523b48be8
Mule Med <muleof2000@gmail.com>
2023-11-23 15:57:27 +0300

    corrections

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,52 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * multiply - multiplies two positive numbers
- * @num1: first number
- * @num2: second number
- *
- * Return: the result of the multiplication
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
  */
-int multiply(int num1, int num2)
-{
-    return num1 * num2;
-}
-
-int is_digit_string(char *str)
-{
-    while (*str)
-    {
-        if (*str < '0' || *str > '9')
-            return 0;
-        str++;
-    }
-    return 1;
-}
-
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
+unsigned long mul;
+int i, j;
+	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{  printf("Error\n");
+			exit(98); }
+		}
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return 98;
-    }
-
-    if (!is_digit_string(argv[1]) || !is_digit_string(argv[2]))
-    {
-        printf("Error\n");
-        return 98;
-    }
-
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
-
-    result = multiply(num1, num2);
-
-    printf("%d\n", result);
-
-    return 0;
+	}
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
 }
-
